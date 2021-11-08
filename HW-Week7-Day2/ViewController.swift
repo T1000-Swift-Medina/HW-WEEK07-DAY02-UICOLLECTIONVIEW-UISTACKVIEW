2ca8d426d22a107d7124e49cdf5514349e2e9984
AfnanTheb <Afnan.Alotaibi.med.s@tuwaiq.edu.sa>
2021-11-08 12:52:17 +0300

    Initial Commit

---
+++ HW-Week7-Day2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW-Week7-Day2
+//
+//  Created by Afnan Theb on 03/04/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
