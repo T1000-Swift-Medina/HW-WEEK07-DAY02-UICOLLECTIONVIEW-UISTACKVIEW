742b0595db95c51306a6463386c3f62e1a662179
shroog <shroogalhwity@gmail.com>
2021-11-08 09:58:42 +0300

    Initial Commit

---
+++ stackViewApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  stackViewApp
+//
+//  Created by شروق فرج سويلم الحويطي on 03/04/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
