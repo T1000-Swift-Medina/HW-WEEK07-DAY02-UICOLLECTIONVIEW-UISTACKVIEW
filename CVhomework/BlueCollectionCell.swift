cb035f0c6fbef6ee5cbf2eca0b5c7cd7901163cf
NUJUD3 <nojoudmohaimmed@hotmail.com>
2021-11-10 00:22:05 +0300

    first editing

---
+++ CVhomework/BlueCollectionCell.swift
@@ -0,0 +1,13 @@
+//
+//  BlueCollectionCell.swift
+//  CVhomework
+//
+//  Created by Nojoud Mohaimmed on 05/04/1443 AH.
+//
+
+import UIKit
+
+class BlueCollectionCell: UICollectionViewCell {
+    
+    @IBOutlet weak var blueBall: UILabel!
+}
--- CVhomework/ViewController.swift
+++ CVhomework/ViewController.swift
@@ -14,12 +14,10 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "mainCell", for: indexPath)as! mainCollectionCell
-///       var dict:[String:Int] = ["A":123, "B": 234, "C": 345]
-//        let index: Int = Int(arc4random_uniform(UInt32(dict.count)))
-//        let randomVal = Array(dict.values)[index] # 123 or 234 or 345
-//        var dic: [String:Int] = ["Blue":0 , "Red":1]
-//        let index: Int = Int(arc4random_uniform(UInt32(dic.count)))
-//        let randomValue = Array(dic.values)[index]
+
+//      let balls : [String:Int] = ["Blue":0 , "Red":1]
+//        let randomBalls = balls.randomElement()
+
         let mainBall = Int.random(in: 0...1)
         var arrayBlue : [Int] = []
         var arrayRed : [Int] = []
@@ -33,7 +31,8 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         return cell
     }
     
-
+    
+    
     @IBOutlet weak var mainCV: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
