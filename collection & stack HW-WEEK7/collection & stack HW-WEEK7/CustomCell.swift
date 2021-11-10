78275b2399c53001ca0fa0ec4c4c4c1958181dbe
Raghad Alahmadi <Raghad.Eid.med.s@tuwaiq.edu.sa>
2021-11-10 22:47:50 +0300

    create coca file + labels to the customcell Class

---
+++ collection & stack HW-WEEK7/collection & stack HW-WEEK7/CustomCell.swift
@@ -0,0 +1,18 @@
+//
+//  CustomCell.swift
+//  collection & stack HW-WEEK7
+//
+//  Created by Raghad Alahmadi on 05/04/1443 AH.
+//
+
+import UIKit
+
+class CustomCell: UICollectionViewCell {
+    @IBOutlet weak var Numbers: UILabel!
+    
+    @IBOutlet weak var oddNum: UILabel!
+    
+    @IBOutlet weak var evenNum: UILabel!
+    
+    
+}
