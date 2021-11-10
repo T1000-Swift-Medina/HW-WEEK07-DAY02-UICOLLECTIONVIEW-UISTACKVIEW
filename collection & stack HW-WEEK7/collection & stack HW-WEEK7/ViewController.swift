454cedde3799bd5a18d95455f0767c69881cad03
Raghad Alahmadi <Raghad.Eid.med.s@tuwaiq.edu.sa>
2021-11-10 23:01:07 +0300

    connect 3 collections with the view controller

--- collection & stack HW-WEEK7/collection & stack HW-WEEK7/ViewController.swift
+++ collection & stack HW-WEEK7/collection & stack HW-WEEK7/ViewController.swift
@@ -7,10 +7,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    @IBOutlet weak var numbersCollection: UICollectionView!
+    @IBOutlet weak var oddCollection: UICollectionView!
+    @IBOutlet weak var evenCollection: UICollectionView!
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 30
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellID", for: indexPath) as! CustomCell
+//        cell.Numbers.text = String([Int])
+        return cell
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        numbersCollection.dataSource = self
+        numbersCollection.delegate = self
+        
+        oddCollection.dataSource = self
+        oddCollection.delegate = self
+        
+        evenCollection.dataSource = self
+        evenCollection.delegate = self
         // Do any additional setup after loading the view.
     }
 
