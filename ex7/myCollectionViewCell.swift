193fd35c33f6a1df92e399168b4c64e750e9b43a
reem alsubhi <Reem.Alsubi.med.s@tuwaiq.edu.sa>
2021-11-11 16:39:02 +0300

    add home work

--- ex7/myCollectionViewCell.swift
+++ ex7/myCollectionViewCell.swift
@@ -10,7 +10,7 @@ import UIKit
 class myCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var title: UILabel!
     
-    @IBOutlet weak var sun: UILabel!
+    @IBOutlet weak var yellow: UILabel!
     
-    @IBOutlet weak var moon: UILabel!
+    @IBOutlet weak var orange : UILabel!
 }
