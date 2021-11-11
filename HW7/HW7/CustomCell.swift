d22e5cc8aa6b01af1a3b3c88fdd5e264a98648d2
AfafSaleh <Afaf.Saleh.med.s@tuwaiq.edu.sa>
2021-11-11 15:33:40 +0300

    HW-07

---
+++ HW7/HW7/CustomCell.swift
@@ -0,0 +1,18 @@
+//
+//  CustomCell.swift
+//  HW7
+//
+//  Created by العــفاف . on 04/04/1443 AH.
+//
+
+import UIKit
+
+class CustomCell: UICollectionViewCell {
+    
+    @IBOutlet weak var Lable1: UILabel!
+    
+    @IBOutlet weak var GreenLable: UILabel!
+    
+    @IBOutlet weak var BlueLable: UILabel!
+    
+}
---
+++ HW7/HW7/ViewController.swift
@@ -0,0 +1,219 @@
+//
+//  ViewController.swift
+//  HW7
+//
+//  Created by العــفاف . on 04/04/1443 AH.
+//
+
+import UIKit
+
+
+
+
+
+class ViewController: UIViewController, UICollectionViewDelegate,UICollectionViewDataSource  {
+    
+    @IBOutlet weak var CollGreen: UICollectionView!
+    @IBOutlet weak var CollBlue: UICollectionView!
+    @IBOutlet weak var CollWhait: UICollectionView!
+    
+    var arrmin: [Int] = []
+    var arrBlue: [Int] = []
+    var arrGreen: [Int] = []
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+    
+        if collectionView == CollWhait {
+            return arrmin.count}
+        else if collectionView == CollBlue {
+            return arrBlue.count}
+        else{
+            return arrGreen.count}
+        
+    }
+    
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellID", for: indexPath) as! CustomCell
+            if (arrmin [indexPath.row] == 0){
+            cell.Lable1.text = "👒"
+            return cell
+        } else {
+            cell.Lable1.text = "👗"
+            return cell}
+        else if collectionView == CollBlue{
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "BlueID", for: indexPath) as! CustomCell
+            cell.Lable1.text = "👗"
+            return cell
+        }else {
+            cell.Lable1.text = "👒"
+            return cell
+        }
+            
+                
+            }
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        if collectionView == CollWhait{
+            print("indexPath:\(indexPath.row) main array : \(arrmin[indexPath.row])")
+            if (arrmin[indexPath.row] == 0){
+                arrBlue.append(arrmin[indexPath.row])
+                arrmin.remove(at: indexPath.row)
+            } else {
+                arrGreen.append(arrmin[indexPath.row])
+                arrmin.remove(at: indexPath.row)
+            } else if collectionView == CollBlue{
+                arrmin.append(arrBlue[indexPath.row])
+                arrBlue.remove(at: indexPath.row)
+            } else {
+                arrmin.append(arrGreen[indexPath.row])
+                arrGreen.remove(at: indexPath.row)
+            }
+        }
+            
+    }
+    
+    override func viewDidLoad() {
+           super.viewDidLoad()
+           // Do any additional setup after loading the view
+   
+        CollGreen.delegate = self
+        CollGreen.dataSource = self
+        CollBlue.delegate = self
+        CollBlue.dataSource = self
+        CollWhait.delegate = self
+        CollWhait.dataSource = self
+        
+   }
+    
+        
+    
+        
+    
+        
+        
+        
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        // Do any additional setup after loading the view
+//
+//
+//}
+//
+//
+    
+    
+    
+    
+    
+    
+    
+    
+    
+//@IBOutlet weak var Blue: UICollectionViewFlowLayout! @IBOutlet weak var Green: UICollectionViewFlowLayout!
+//    var ArrayBlue: [Int] = []
+//    var ArrayGreen: [Int] = []
+//    var ArrayBlueGreen: [Int] = []
+//
+//
+//
+//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//
+//
+//        if collectionView == CV {return 50}
+//        else if { collectionView == Blue {return ArrayBlue.count }
+//            else do {return Green.count}
+//
+//
+//        }
+//
+////        let numbers = [0...100]
+////        print(numbers)
+//
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier:"cellID" , for: indexPath) as! CustomCell
+//        cell.Lable1.text = String(indexPath.row + 1)
+//        _ = Int.random(in: 0...1)
+//
+////        func sortedEvenOddNumbers(_ numbers: [Int]) -> (evens: [Int], odds: [Int]) {
+////            var evens = [Int]()
+////            var odds = [Int]()
+////            for number in numbers {
+////                if number % 2 == 0 {
+////                    evens.append(number)
+////
+////        } else {
+////                    odds.append(number)
+////                }
+////        }
+////            return (evens, odds)
+////        }
+////
+////        cell.backgroundColor = .systemYellow
+////        return cell
+////    }
+////
+////
+////
+////
+////
+//
+//        weak var CV: UICollectionView!
+//
+//
+//        func viewDidLoad() {
+//        super.viewDidLoad()
+//        // Do any additional setup after loading the view.
+//        CV.delegate = self
+//        CV.dataSource = self
+//    }
+//
+//}
+//
+//
+//}
+//var ArrayBlue: [Int] = []
+//var ArrayGreen: [Int] = []
+//var ArrayBG: [Int] = []
+//var selectBlue: [Int] = []
+//var selectGreen: [Int] = []
+//
+//func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//    if collectionView == GreenBlue {return 50}
+//    else if collectionView == Blue {return ArrayBlue.count}
+//    else {return ArrayGreen.count }
+//}
+//
+//func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellID", for: indexPath)
+//return cell }
+//
+//override func viewDidLoad() {
+//    super.viewDidLoad()
+//}
+//}
+
+
+//        }
+//            if(n as! Int % 2 == 0){
+//                print("\(n) is even")
+//            }else{nn
+//                print("\(n) is odd")
+//            }
+//        }
+//    }
+    
