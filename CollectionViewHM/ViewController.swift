435cb2fbe8c369932edf22c0c04c2681c49d00e2
Razan <irazan.m24@gmail.com>
2021-11-11 15:51:31 +0300

    did select function, did reload function,edit the constrains, if statement for all arrays

--- CollectionViewHM/ViewController.swift
+++ CollectionViewHM/ViewController.swift
@@ -33,34 +33,75 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
         if collectionView == cvMain {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MainCell", for: indexPath) as! CustomCell
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MainCell", for: indexPath) as! CustomCell
             
-//            if (arrRed[indexPath.row] == 0){
-//                cell.layer.backgroundColor.black.cgColor
-//                cell.layer.cornerRadius = cell.frame.height/2
-//                return arrRed.count
-//            }
-            if (arrRed[indexPath.row] == 0){
-//                cell.CellNum.backgroundColor = red
+            //            if (arrRed[indexPath.row] == 0){
+            //                cell.layer.backgroundColor.black.cgColor
+            //                cell.layer.cornerRadius = cell.frame.height/2
+            //                return arrRed.count
+            //            }
+            if (arrMixedBall[indexPath.row] == 0){
+                //                cell.CellNum.backgroundColor = red
                 cell.CellNum.text = "❤️"
-            } else if (arrBlue[indexPath.row] == 1) {
+            } else {
                 cell.CellNum.text = "💙"
             }
             return cell
         } else if BlueCV == collectionView {
             let blue = collectionView.dequeueReusableCell(withReuseIdentifier: "BlueCell", for: indexPath) as! CustomCell
-            
+            blue.blueHeart.text = "💙"
+            return blue
+        } else {
+            let red = collectionView.dequeueReusableCell(withReuseIdentifier: "RedCell", for: indexPath) as! CustomCell
+            red.RedHeart.text = "❤️"
+            return red
         }
-    
-    }
-    
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
         
     }
-
-
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        if collectionView == cvMain {
+            if (arrMixedBall[indexPath.row] == 0) {
+                arrMixedBall.remove(at: indexPath.row)
+                arrRed.append(arrMixedBall[indexPath.row])
+            }else {
+                arrMixedBall.remove(at: indexPath.row)
+                arrBlue.append(arrMixedBall[indexPath.row])
+            }
+        }
+        else if collectionView == RedCV {
+            arrMixedBall.append(arrRed[indexPath.row])
+            arrRed.remove(at: indexPath.row)
+        } else {
+            arrMixedBall.append(arrBlue[indexPath.row])
+            arrBlue.remove(at: indexPath.row)
+        }
+        
+        cvMain.reloadData()
+        BlueCV.reloadData()
+        RedCV.reloadData()
+        
+        
+        
+    }
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        for _ in 0...50 {
+            arrMixedBall.append(Int.random(in: 0...1))
+        }
+        // Do any additional setup after loading the view.
+        cvMain.delegate = self
+        cvMain.dataSource = self
+        BlueCV.delegate = self
+        BlueCV.dataSource = self
+        RedCV.delegate = self
+        RedCV.dataSource = self
+    }
+    
+    
 }
 
