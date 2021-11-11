cc5e38398c699ca1bd7fbcf56d087ac2853bd9d4
Raghad Alahmadi <Raghad.Eid.med.s@tuwaiq.edu.sa>
2021-11-11 12:55:00 +0300

    didSelecte function

--- collection & stack HW-WEEK7/collection & stack HW-WEEK7/ViewController.swift
+++ collection & stack HW-WEEK7/collection & stack HW-WEEK7/ViewController.swift
@@ -14,31 +14,131 @@ class ViewController: UIViewController , UICollectionViewDelegate, UICollectionV
     @IBOutlet weak var evenCollection: UICollectionView!
     
     
+    var numbersArray : [Int] = []
+    var oddNumbersArray: [Int] = []
+    var evenNumbersArray: [Int] = []
+//    var selectedRedBallsArray: [Int] = []
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 30
+//        return 60
+        
+        
+        if collectionView == numbersCollection {
+            return numbersArray.count }
+         else if collectionView == oddCollection {
+             return oddNumbersArray.count}
+         else {
+             return evenNumbersArray.count}
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellID", for: indexPath) as! CustomCell
-//        cell.Numbers.text = String([Int])
-        return cell
+        if  collectionView == numbersCollection  {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellID", for: indexPath) as! CustomCell
+                cell.Numbers.text = String(numbersArray[indexPath.row])
+            
+            if (collectionView == numbersCollection && numbersArray[indexPath.row] % 2 == 0) {
+                evenNumbersArray[indexPath.row]
+//                cell.Numbers.text 
+                
+                
+            }
+            else {
+                oddNumbersArray[indexPath.row]
+            }
+            
+//        let numbers = Int(60)
+            return cell
+            
+        }
+            
+            else if  collectionView == oddCollection {
+               let oddCell =  collectionView.dequeueReusableCell(withReuseIdentifier: "ocellID", for: indexPath)as! CustomCell
+//                oddNumbersArray[indexPath.row]
+                oddCell.oddNum.text = String(oddNumbersArray[indexPath.row])
+                //[indexPath.row]
+                
+         return oddCell
+                
+     }
+        
+        else {
+            let evenCell =   collectionView.dequeueReusableCell(withReuseIdentifier: "ecellID", for: indexPath)as! CustomCell
+//           evenNumbersArray[indexPath.row]
+            evenCell.evenNum.text = String(oddNumbersArray[indexPath.row])
+           
+            return evenCell
+            
+        }
     }
     
+    func collectionView(_ collectionView: UICollectionView,didSelectItemAt indexPath: IndexPath){
+        
+        if collectionView == numbersCollection {
+//           oddCollection.append(numbersArray[indexPath.row])
+//            numbersArray.remove(at: indexPath.row)
+          
+            if numbersArray [indexPath.row] % 2 == 0{
+            evenNumbersArray.append(numbersArray[indexPath.row])
+            numbersArray.remove(at: indexPath.row)
+            
+            }
+            else{
+                oddNumbersArray.append(numbersArray[indexPath.row])
+               numbersArray.remove(at: indexPath.row)
+            }
+            numbersCollection.reloadData()
+              evenCollection.reloadData()
+            oddCollection.reloadData()
+            
+        }
+            
+            else if (collectionView == evenCollection) {
+                
+          
+
+               numbersArray.append(evenNumbersArray[indexPath.row])
+               evenNumbersArray.remove(at: indexPath.row)
+                numbersCollection.reloadData()
+                  evenCollection.reloadData()
+                 oddCollection.reloadData()
+            }
+        else{
+
+         
+         numbersArray.append(oddNumbersArray[indexPath.row])
+           oddNumbersArray.remove(at: indexPath.row)
+           numbersCollection.reloadData()
+            evenCollection.reloadData()
+           oddCollection.reloadData()
+        }
+        collectionView.reloadData()
+            
+        }
+        
+     
+
+   
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         numbersCollection.dataSource = self
         numbersCollection.delegate = self
-        
+
         oddCollection.dataSource = self
         oddCollection.delegate = self
-        
+
         evenCollection.dataSource = self
         evenCollection.delegate = self
         // Do any additional setup after loading the view.
+        
+        for i in 1...100 {
+            numbersArray.append(i)
+        }
     }
 
-
 }
+
+
 
