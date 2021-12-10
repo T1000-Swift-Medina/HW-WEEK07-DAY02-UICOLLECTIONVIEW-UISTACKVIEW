98309c63db0f4e0da0112411801b4b567efeb991
BadrAljohani <Badr.Aljohani.med.s@tuwaiq.edu.sa>
2021-12-10 18:40:29 +0300

    The requested modification has been made

--- Stack View2/ViewController.swift
+++ Stack View2/ViewController.swift
@@ -3,7 +3,7 @@ import UIKit
 class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
     
     var arrMain : [Int] = []
-    var arrBlue : [Int] = []
+    var arrBlue : [Int] = [] 
     var arrRed : [Int] = []
     
     @IBOutlet weak var CVMain: UICollectionView!
@@ -33,36 +33,25 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         CVBlue.dataSource = self
         
         
-        for n in 0...30 {
+        for n in 1...50 {
             arrMain.append(n)
         }
-        
-        
-        //            CVBlue.register(myCellBlue.self, forCellReuseIdentifier: "myCellBlue")
-        //        CVBlue.register(BlueCollectionCell.self, forCellWithReuseIdentifier: "myCellBlue")
-        //            CVRed.register(myCellRed.self,  forCellReuseIdentifier: "myCellRed")
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
-//        print (collectionView)
         
         if (collectionView == CVMain) {
             let cell = CVMain.dequeueReusableCell(withReuseIdentifier: "myMainCell", for: indexPath) as! CellOne
             cell.myMainCellLabel.text = String(arrMain[indexPath.row])
             
             if indexPath.row % 2 == 0 {
-                cell.backgroundColor = .blue
-                // try return in condetion
-//            } else if indexPath.row % 2 != 0 {
+                cell.backgroundColor = .blue.withAlphaComponent(0.7)
             } else {
-                cell.backgroundColor = .red
-                cell.layer.cornerRadius = 24
-                
+                cell.backgroundColor = .red.withAlphaComponent(0.7)
             }
+            cell.layer.cornerRadius = 24
             
-            //            cell.backgroundColor = .red
-            //            cell.layer.cornerRadius = 24
             return cell
         }
         
@@ -70,29 +59,31 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
             let cell = CVBlue.dequeueReusableCell(withReuseIdentifier: "myCellBlue", for: indexPath) as! BlueCollectionCell
             let cred = arrBlue[indexPath.row]
             cell.myCellBlue.text = String(cred)
+            cell.backgroundColor = .white.withAlphaComponent(0.4)
+            cell.layer.cornerRadius = 5
             return cell
         }
         else {
             let cell = CVRed.dequeueReusableCell(withReuseIdentifier: "myCellRed", for: indexPath) as! RedCollectionCell
             let cred = arrRed[indexPath.row]
             print(arrRed[indexPath.row])
-//            cell.myCellRed.text = String(cred)
             cell.testLbl.text = String(cred)
-            
-
+            cell.backgroundColor = .white.withAlphaComponent(0.4)
+            cell.testLbl.textColor = .black
+            cell.layer.cornerRadius = 5
             return cell
+        }
+    }
+
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        
+        // MARK: Animate all the cells
+        UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.25, initialSpringVelocity: 0.25, options: [.curveEaseInOut, .allowUserInteraction]) {
+            cell.transform = CGAffineTransform(scaleX: 0.9, y: 0.9)
         }
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath){
-        //        if (collectionView == CVMain) {
-        //            let tappedNumber = arrMain[indexPath.row]
-        //            if tappedNumber % 2 == 0 {
-        //                arrBlue.append(tappedNumber)
-        //                arrMain.remove(at: indexPath.row)
-        //            } else {
-        //
-        //            }
         if (collectionView == CVMain) {
             let tappedNumber = arrMain[indexPath.row]
             if tappedNumber % 2 == 0 {
@@ -105,8 +96,8 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
             }
             
         }else if (collectionView == CVBlue){
-            arrBlue.remove (at: indexPath.row)
             arrMain.append(arrBlue[indexPath.row])
+            arrBlue.remove (at: indexPath.row)
         } else {
             arrMain.append(arrRed[indexPath.row])
             arrRed.remove(at: indexPath.row)
@@ -118,15 +109,13 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     }
 }
 
-
-
 class BlueCollectionCell: UICollectionViewCell {
     @IBOutlet weak var myCellBlue: UILabel!
     
 }
 
 class RedCollectionCell : UICollectionViewCell {
-//    @IBOutlet weak var myCellRed: UILabel!
+    
     @IBOutlet weak var testLbl: UILabel!
     
 }
